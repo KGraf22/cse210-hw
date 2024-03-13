934a9962b97a5061fbfa95d7cce22027757789a2
KGraf22 <kbgraf22@gmail.com>
2024-03-13 12:57:47 -0700

    Develp03 start

--- prove/Develop03/Program.cs
+++ prove/Develop03/Program.cs
@@ -1,9 +1,99 @@
 using System;
+using System.Security.Cryptography.X509Certificates;
 
 class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Develop03 World!");
+        public class Scripture
+        {
+            private Reference _reference;
+            private List<Word> _words;
+
+            public ScriptureReference(string Reference,string text)
+            {
+
+            }
+
+
+            public void HideRandomWords(int numberToHide)
+            {
+
+            }
+
+            public string GetDisplayText()
+            {
+
+            }
+  
+            public void IsCompletelyHidden()
+            {
+
+            }
+            public void Display()
+            {
+
+            }
+        }
+
+        public class Reference
+        {
+            private string _book;
+            private int _chapter;
+            private int _verse;
+            private int _endVerse;
+
+            public Reference(string _book,int _chapter, int _verse)
+            {
+
+            }
+
+            public Reference( string _book, int chapter, int _startVerse, int endVerse)
+            {
+
+            }
+
+            public string GetDisplayText()
+            {
+        
+            }
+        }
+
+
+        public class Word
+        {
+            private string _text;
+            private bool _isHidden;
+
+            public Word(string _text)
+            {
+
+            }
+
+            public void Hide()
+            {
+
+            }
+
+            public void Show()
+            {
+
+            }
+
+            public bool IsHidden()
+            {
+
+            }
+
+            public string GetDisplayText()
+            {
+
+            }
+
+        }
+
+
+
+
     }
 }
