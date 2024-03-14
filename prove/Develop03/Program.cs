cbcf55886aada45c3035d3390c43c008fcea1c3a
KGraf22 <kbgraf22@gmail.com>
2024-03-14 14:32:38 -0700

    Moved word class to top and worked on filling in word class

--- prove/Develop03/Program.cs
+++ prove/Develop03/Program.cs
@@ -1,99 +1,111 @@
 using System;
-using System.Security.Cryptography.X509Certificates;
+using System.Collections.Generic;
 
-class Program
+public class Word
 {
-    static void Main(string[] args)
+    private string _text;
+    private bool _isHidden;
+
+    public Word(string _text)
     {
-        public class Scripture
-        {
-            private Reference _reference;
-            private List<Word> _words;
+        _text = text;
+        _isHidden = false;
+    }
 
-            public ScriptureReference(string Reference,string text)
-            {
+    public void Hide()
+    {
+        _isHidden = true;
+    }
 
-            }
+    public void Show()
+    {
+        _isHidden = false;
+    }
+
+    public bool IsHidden()
+    {
+        return _isHidden;
+
+    }
+
+    public string GetDisplayText()
+    {
+        if (_isHidden)
+            return "_";
+        else 
+            return _text;
+
+    }
+
+}    
+public class Scripture
+{
+    private Reference _reference;
+    private List<Word> _words;
+
+    public Scripture(Reference reference,string text)
+    {
+        _reference = reference;
+        _words = new List<Word>();
+
+    }
 
 
-            public void HideRandomWords(int numberToHide)
-            {
+    public void HideRandomWords(int numberToHide)
+    {
 
-            }
+    }
 
-            public string GetDisplayText()
-            {
+    public string GetDisplayText()
+    {
 
-            }
+    }
   
-            public void IsCompletelyHidden()
-            {
+    public void IsCompletelyHidden()
+    {
 
-            }
-            public void Display()
-            {
-
-            }
-        }
-
-        public class Reference
-        {
-            private string _book;
-            private int _chapter;
-            private int _verse;
-            private int _endVerse;
-
-            public Reference(string _book,int _chapter, int _verse)
-            {
-
-            }
-
-            public Reference( string _book, int chapter, int _startVerse, int endVerse)
-            {
-
-            }
-
-            public string GetDisplayText()
-            {
-        
-            }
-        }
-
-
-        public class Word
-        {
-            private string _text;
-            private bool _isHidden;
-
-            public Word(string _text)
-            {
-
-            }
-
-            public void Hide()
-            {
-
-            }
-
-            public void Show()
-            {
-
-            }
-
-            public bool IsHidden()
-            {
-
-            }
-
-            public string GetDisplayText()
-            {
-
-            }
-
-        }
-
-
+    }
+    public void Display()
+    {
 
 
     }
+
+public class Reference
+{
+    private string _book;
+    private int _chapter;
+    private int _verse;
+    private int _endVerse;
+
+    public Reference(string _book, int _chapter, int _verse)
+    {
+        _book = book;
+        _chapter = chapter;
+        _verse = verse;
+
+    }
+
+    public Reference( string _book, int chapter, int _startVerse, int endVerse)
+    {
+        _book = book;
+        _chapter = chapter;
+        _verse = startVerse;
+        _endVerse = endVerse;
+
+    }
+
+    public string GetDisplayText()
+    {
+        
+    }
+ 
+
+public class Program
+{
+    public static void Main(string[] args)
+    {
+
+    }
 }
+        
