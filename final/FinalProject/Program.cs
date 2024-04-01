d98cbcd0832d40cda4c20987b859a8e7dbe817df
KGraf22 <kbgraf22@gmail.com>
2024-04-01 12:32:49 -0700

    started on final project getting the customer files

--- final/FinalProject/Program.cs
+++ final/FinalProject/Program.cs
@@ -1,9 +1,19 @@
 using System;
-
+using Microsoft.VisualBasic.FileIO;
 class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello FinalProject World!");
+        string customersListFilePath = "customers-list.csv";
+        string customerSalesFilePath = "customer_sales.csv";
+
+        ReadCSV(customersListFilePath);
+
+        ReadCSV(customerSalesFilePath);
+    }
+
+    static void ReadCSV (string filePath)
+    {
+
     }
 }
